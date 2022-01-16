55bc0c4147820d91a436a1950f63da4ee2d1c960
Kamil Chec <k.chec.prv@gmail.com>
2022-01-16 13:43:50 +0100

    Add full working eco algorithm

--- src/ecosystemAlgorithm.py
+++ src/ecosystemAlgorithm.py
@@ -2,23 +2,182 @@ import numpy as np
 import neuralNetwork as nn
 import dataset as d
 import ecoParams as param
+import sys
+import math
+from tqdm import tqdm
 
 class EcosystemAlgorithm:
+    #TODO merge plant_progress with best_plan and so on
     def __init__(self, NeuralNetwork):
         self.NeuralNetwork = NeuralNetwork
         self.best_plant = None
         self.best_herbivore = None
         self.best_predator = None
-      
+        self.plant_progress = []
+        self.herbivore_progress = []
+        self.predator_progress = []
+        
+        self.deaths = 0
+        self.mutations = 0
+        
+        self.mcp_explore = 1.5
+        self.mcp_exploitation = 0
+        
+    def exploration_exploitation_change(self):
+        if self.mcp_explore > 0.1: self.mcp_explore -= 0.1
+        if self.mcp_exploitation < 1.5: self.mcp_exploitation += 0.1
+        
+     #TODO zrobić wypisanie na sam koniec z tablicy   
+    def print_loss(self, Plants, Herbivores, Predators, iteration, first_use=False):
+        best_plant = self.find_best_organism(Plants)[0]    
+        best_herbivore = self.find_best_organism(Herbivores)[0]
+        best_predator = self.find_best_organism(Predators)[0]
+        
+        self.plant_progress.append(best_plant)
+        self.herbivore_progress.append(best_herbivore)
+        self.predator_progress.append(best_predator)
+        
+        mode = 'w' if first_use else 'a'
+        with open('res/ecoData.txt', mode) as file:
+                file.write(
+                    "Loss after iteration {} :\tplants: {}\therbivores: {}\tpredators: {}\n".format(
+                        iteration, best_plant, best_herbivore, best_predator))
+        
+    def animal_mutation(self, Animals, mutation_param):
+        for animal in Animals:
+            if np.random.random() < mutation_param:
+                self.mutations += 1
+                mutation_point = np.random.randint(animal.mcp.size - 1)
+                animal.mcp[mutation_point] = np.random.uniform(
+                    param.mcp_scope[0], param.mcp_scope[1])
+        
+        return Animals
+                  
+    def plant_mutation(self, Plants):
+        for plant in Plants:
+            if np.random.random() < param.mutation_pl:
+                self.mutations += 1
+                mutation_point = np.random.randint(plant.position.size - 1)
+                plant.position[mutation_point] = np.random.uniform(-5, 5)
+                  
+        return Plants
+        
+    def animal_crossover(self, Object, Animals, Preys, n):
+        if Animals.size < n:
+            offspring = self.crossover(Animals, n)
+            for ii, animal in enumerate(offspring):
+                adaptation = self.count_adaptation(animal[0])
+                mcp = animal[1]
+                prey = np.random.choice(Preys)
+                offspring[ii] = Object(animal[0], adaptation, mcp, 0, prey)
+                
+            return np.concatenate((Animals, offspring))
+        else:
+            return Animals
+        
+    def plant_crossover(self, Plants):
+        if Plants.size < param.n_pl:
+            offspring = self.crossover(Plants, param.n_pl)
+            for ii, plant in enumerate(offspring):
+                adaptation = self.count_adaptation(plant)
+                offspring[ii] = Plant(plant, adaptation)
+                
+            for p in offspring: p.set_size(self.count_plants_size(p, offspring))
+
+            return np.concatenate((Plants, offspring))
+        else:
+            return Plants
+     
+    def crossover(self, Organisms, n):
+        refills_numb = n - Organisms.size
+        offspring = np.empty(refills_numb, dtype=object)
+        probability = self.probability_of_selecting(Organisms)
+        for i in range(refills_numb):
+            crossing_point = np.random.randint(Organisms[0].position.size)
+            mother = np.random.choice(Organisms, p=probability)
+            father = np.random.choice(Organisms, p=probability)
+                            
+            position = np.concatenate(  
+                (father.position[:crossing_point], mother.position[crossing_point:]))
+            if hasattr(mother, 'mcp'):
+                crossing_point = np.random.randint(Organisms[0].mcp.size)
+                mcp =  np.concatenate(
+                    (father.mcp[:crossing_point], mother.mcp[crossing_point:]))
+                offspring[i] = [position, mcp]
+            else: offspring[i] = position
+                                    
+        return offspring             
+                
+    def probability_of_selecting(self, Organisms):
+        log_adaptation_sum = 0
+        log_adaptation_sum = sum([math.exp(-o.adaptation*2) for o in Organisms])
+        probabilities = [math.exp(-o.adaptation*2)/log_adaptation_sum for o in Organisms]
+        norm_factor = 1 / sum(probabilities) 
+        normalised = [norm_factor * p for p in probabilities] 
+        
+        return normalised
+            
+        
+    def herbivore_predator_simulation(self, Herbivores, Predators):
+        for predator in Predators:
+            if predator.adaptation < predator.prey.adaptation:
+                Herbivores = self.dies(predator.prey, Herbivores)
+                predator.set_life(predator.life + 1)
+                predator.set_prey(np.random.choice(Herbivores))
+            else:
+                self.run(predator.prey)
+                predator.set_position(predator.prey.position)
+                predator.set_life(predator.life - 1)
+                if predator.life < 0:
+                    Predators = self.dies(predator, Predators)
+                        
+        return Herbivores, Predators
+    
+    def run(self, runaway):
+        v = np.random.uniform(-0.1, 0.1, size=runaway.position.size)
+        runaway.set_position(runaway.position + v)
+    
+    def plant_herbivore_simulation(self, Plants, Herbivores):
+        for herbivore in Herbivores:
+            try:
+                Plants = self.eat_plants(herbivore, Plants, param.nutrition_req)
+            except ValueError as err:
+                sys.exit(err.args)
+        
+        return Plants
+        
+    def eat_plants(self, herbivore, Plants, food_req):
+        effect = herbivore.prey.size - food_req
+        
+        if effect <= 0:
+            survived = self.dies(herbivore.prey, Plants)
+            if survived.size:
+                herbivore.set_prey(np.random.choice(survived))
+                return self.eat_plants(herbivore, survived, np.absolute(effect))
+            else: raise ValueError('All the plants are eaten and the herbivores want more')
+        else: 
+            herbivore.prey.set_size(effect)
+            
+        return Plants
+        
+    def dies(self, specimen, Organisms):
+        self.deaths += 1
+        survived = np.array([o for o in Organisms if o != specimen])
+        del specimen
+        
+        return survived
+            
+    
     def perform_movement(self, Animals, best_so_far, best_prey):
          for animal in Animals:
-            velocity = self.count_velocity(animal, best_so_far, best_prey)
+            # velocity = self.count_velocity(animal, best_so_far, best_prey)
+            velocity = self.count_velocity_PSO(animal, best_so_far, best_prey)
             animal.set_velocity(velocity)
             position = animal.position + animal.velocity
             animal.set_position(position)
-     
+            
     def count_velocity(self, Animal, best_animal_so_far, best_prey_so_far):
-        r = np.random.rand()
+        r = np.random.uniform(0, 0.2)
         pt_prev_v = Animal.mcp[0] * r * Animal.velocity
         pt_best = Animal.mcp[1] * r * (Animal.best_organism[1] - Animal.position)
         pt_best_so_far = Animal.mcp[2] * r * (best_animal_so_far[1] - Animal.position)
@@ -27,9 +186,23 @@ class EcosystemAlgorithm:
         else: pt_best_neighbour = 0
         pt_assign_prey = Animal.mcp[4] * r * (Animal.prey.position - Animal.position)
         pt_best_prey = Animal.mcp[5] * r * (best_prey_so_far[1] - Animal.position)
-        
+                 
         return pt_prev_v + pt_best + pt_best_so_far + pt_best_neighbour + pt_assign_prey + pt_best_prey
+    
+    def count_velocity_PSO(self, Animal, best_animal_so_far, best_prey_so_far):
+        r = np.random.rand(6)
+        w = 0.8
+        pt_prev_v = w * Animal.velocity
+        pt_best = self.mcp_explore * r[0] * (Animal.best_organism[1] - Animal.position)
+        pt_best_so_far = self.mcp_exploitation * r[1] * (best_animal_so_far[1] - Animal.position)
+        if Animal.best_neighbour is not None:
+            pt_best_neighbour = 0.3 * r[2] * (Animal.best_neighbour[1] - Animal.position)
+        else: pt_best_neighbour = 0
+        pt_assign_prey =  self.mcp_explore * r[3] * (Animal.prey.position - Animal.position)
+        pt_best_prey =  self.mcp_explore * r[4] * (best_prey_so_far[1] - Animal.position)
         
+        return pt_prev_v + pt_best + pt_best_so_far + pt_best_prey + pt_assign_prey + pt_best_neighbour
+    
     def save_best_neighbour(self, Animals):
         for animal in Animals:
             rest = np.array([n for n in Animals if n != animal])
@@ -81,15 +254,26 @@ class EcosystemAlgorithm:
         
         return plants, herbivores, predators
     
+    def set_adaptation_for_all(self, Plants, Herbivores, Predators):
+        self.set_adaptation(Plants)
+        self.set_adaptation(Herbivores)
+        self.set_adaptation(Predators)
+    
+    def set_adaptation(self, Organisms):
+        for organism in Organisms:
+            adaptaion = self.count_adaptation(organism.position)
+            organism.set_adaptation(adaptaion)
+    
+    #TODO concat predator and herbivore population creation
     def create_predator_population(self, Herbiovores):
         predators = np.empty(shape=(param.n_pr,), dtype=object)
         
         for i in range(param.n_pr):
             position = self.deploy_position()
             adaptation = self.count_adaptation(position)
-            mcp = np.random.uniform(-0.5, 2, 6)
+            mcp = np.random.uniform(param.mcp_scope[0], param.mcp_scope[1], 6)
             prey = np.random.choice(Herbiovores)
-            predators[i] = Predator(i, position, adaptation, mcp , 0, prey)
+            predators[i] = Predator(position, adaptation, mcp , 0, prey)
             
         return predators
     
@@ -99,9 +283,9 @@ class EcosystemAlgorithm:
         for i in range(param.n_he):
             position = self.deploy_position()
             adaptation = self.count_adaptation(position)
-            mcp = np.random.uniform(-0.5, 2, 6)
+            mcp = np.random.uniform(param.mcp_scope[0], param.mcp_scope[1], 6)
             prey = np.random.choice(Plants)
-            herbivores[i] = Herbivore(i, position, adaptation, mcp, 0, prey)
+            herbivores[i] = Herbivore(position, adaptation, mcp, 0, prey)
             
         return herbivores
     
@@ -111,7 +295,7 @@ class EcosystemAlgorithm:
         for i in range(param.n_pl):
             position = self.deploy_position()
             adaptation = self.count_adaptation(position)
-            plants[i] = Plant(i, position, adaptation)
+            plants[i] = Plant(position, adaptation)
         
         for p in plants: p.set_size(self.count_plants_size(p, plants))
         
@@ -123,7 +307,7 @@ class EcosystemAlgorithm:
             for p in network_parameters: position = np.append(position, np.ravel(p))
             
             return position
-            
+          
     def count_plants_size(self, plant, plants):
         sum_adaptation = sum(p.adaptation for p in plants)
         size = plant.adaptation / sum_adaptation * param.n_pl
@@ -152,19 +336,18 @@ class EcosystemAlgorithm:
             
 
 class Organism: 
-    def __init__(self, id, position, adaptation):
-        self.id = id
+    def __init__(self, position, adaptation):
         self.position = position
         self.adaptation = adaptation
         self._best_organism = None
 
     @property
     def best_organism(self):
-        return self._best_organism
+        return self._best_organism     
     
     def save_best_organism(self):
         if self.best_organism is not None:
-            if self.best_organism[0] < self.adaptation:
+            if self.best_organism[0] > self.adaptation:
                 self._best_organism = [self.adaptation, self.position]
         else:
             self._best_organism = [self.adaptation, self.position]
@@ -176,20 +359,20 @@ class Organism:
         self.position = position
 
 class Plant(Organism):
-    def __init__(self, id, position, adaptation):
-        super().__init__(id, position, adaptation)
+    def __init__(self, position, adaptation):
+        super().__init__(position, adaptation)
         self._size = None
         
     @property
     def size(self):
         return self._size
-        
+    
     def set_size(self, size):
         self._size = size
         
 class Animal(Organism):
-    def __init__(self, id, position, adaptation, mcp, velocity, prey):
-        super().__init__(id, position, adaptation)
+    def __init__(self,position, adaptation, mcp, velocity, prey):
+        super().__init__(position, adaptation)
         self.velocity = velocity
         self.prey = prey
         self.mcp = mcp
@@ -205,15 +388,21 @@ class Animal(Organism):
     def set_velocity(self, velocity):
         self.velocity = velocity
         
+    def set_prey(self, prey):
+        self.prey = prey
+        
 class Herbivore(Animal):
-    def __init__(self, id, position, adaptation, mcp, velocity, prey):
-        super().__init__(id, position, adaptation, mcp, velocity, prey)       
+    def __init__(self,position, adaptation, mcp, velocity, prey):
+        super().__init__(position, adaptation, mcp, velocity, prey)       
     
 
 class Predator(Animal):
-    def __init__(self, id, position, adaptation, mcp, velocity, prey):
-        super().__init__(id, position, adaptation, mcp, velocity, prey)
+    def __init__(self,position, adaptation, mcp, velocity, prey):
+        super().__init__(position, adaptation, mcp, velocity, prey)
         self.life = param.life
+        
+    def set_life(self, life):
+        self.life = life
         
 def main():
     data = d.Dataset()
@@ -221,13 +410,37 @@ def main():
     neural_network = nn.NeuralNetwork(input, output)
     eco = EcosystemAlgorithm(neural_network)
 
-    plants = eco.create_plant_population()
-    herbivores = eco.create_herbivore_population(plants)
-    predators = eco.create_predator_population(herbivores)
+    plants, herbivores, predators = eco.create_population()
 
     eco.save_bests(plants, herbivores, predators)
+    
+    for i in tqdm(range(param.alg_iter)):
+        eco.perform_movement(herbivores, eco.best_herbivore, eco.best_plant)
+        eco.perform_movement(predators, eco.best_predator, eco.best_herbivore)
+        
+        if i % param.interactions == 0:
+            eco.exploration_exploitation_change()
+            plants = eco.plant_herbivore_simulation(plants, herbivores)
+            herbivores, predators = eco.herbivore_predator_simulation(herbivores, predators)
 
-    eco.perform_movement(herbivores, eco.best_herbivore, eco.best_plant)
+            plants = eco.plant_crossover(plants)
+            herbivores = eco.animal_crossover(Herbivore, herbivores, plants, param.n_he)
+            predators = eco.animal_crossover(Predator, predators, herbivores, param.n_pr)
+            
+            plants = eco.plant_mutation(plants)
+            herbivores = eco.animal_mutation(herbivores, param.mutation_he)
+            predators = eco.animal_mutation(predators, param.mutation_pr)
+    
+            eco.save_bests(plants, herbivores, predators)
+            
+        if i % param.save_to_file == 0 or i == param.alg_iter - 1:
+            first_use = True if i == 0 else False 
+            eco.print_loss(plants, herbivores, predators, i, first_use)
+        
+        eco.set_adaptation_for_all(plants, herbivores, predators)
+        eco.save_bests(plants, herbivores, predators)
+        
+    print('deaths: {}\tmutations: {}'.format(eco.deaths, eco.mutations))
 
 if __name__ == "__main__":
     main()
--- src/main.py
+++ src/main.py
@@ -12,31 +12,31 @@ def main():
     partical_optimalization =  pso.ParticleSwarmOptimization(10000, 0.8, 1, 0, neural_network)
     
     
-    chromosomes = genetic_algorithm.create_population()
-    genetic_algorithm.set_adaptation(chromosomes)
-    genetic_algorithm.print_loss(chromosomes, 'init', first_use=True)
-    for i in tqdm(range(20000)):
-        crossedOffspring = genetic_algorithm.crossover(chromosomes)
-        mutatedOffspring = genetic_algorithm.mutation(crossedOffspring)
-        genetic_algorithm.set_new_population(mutatedOffspring, chromosomes)
-        if i % 1000 == 0:
-                genetic_algorithm.print_loss(chromosomes, i)
-    print(genetic_algorithm.best_chromosome(chromosomes))
-    genetic_algorithm.plot_evolution_of_adaptation()
-    data.plot_decision_boundary(lambda x: neural_network.predict(x))
+    # chromosomes = genetic_algorithm.create_population()
+    # genetic_algorithm.set_adaptation(chromosomes)
+    # genetic_algorithm.print_loss(chromosomes, 'init', first_use=True)
+    # for i in tqdm(range(20000)):
+    #     crossedOffspring = genetic_algorithm.crossover(chromosomes)
+    #     mutatedOffspring = genetic_algorithm.mutation(crossedOffspring)
+    #     genetic_algorithm.set_new_population(mutatedOffspring, chromosomes)
+    #     if i % 1000 == 0:
+    #             genetic_algorithm.print_loss(chromosomes, i)
+    # print(genetic_algorithm.best_chromosome(chromosomes))
+    # genetic_algorithm.plot_evolution_of_adaptation()
+    # data.plot_decision_boundary(lambda x: neural_network.predict(x))
     
-    # population = partical_optimalization.create_population()
-    # partical_optimalization.set_all_parameters(population)
-    # for i in tqdm(range(100)):
-    #     partical_optimalization.set_all_parameters(population)
-    #     partical_optimalization.perform_movement(population)
-    #     if i % 10 == 0:
-    #         if (i == 0): 
-    #             partical_optimalization.print_loss(i, first_use=True)
-    #         else:
-    #             partical_optimalization.print_loss(i)
-    #         if partical_optimalization.c1 > 0.1: partical_optimalization.c1 -= 0.1
-    #         partical_optimalization.c2 += 0.1
+    population = partical_optimalization.create_population()
+    partical_optimalization.set_all_parameters(population)
+    for i in tqdm(range(100)):
+        partical_optimalization.set_all_parameters(population)
+        partical_optimalization.perform_movement(population)
+        if i % 10 == 0:
+            if (i == 0): 
+                partical_optimalization.print_loss(i, first_use=True)
+            else:
+                partical_optimalization.print_loss(i)
+            if partical_optimalization.c1 > 0.1: partical_optimalization.c1 -= 0.1
+            partical_optimalization.c2 += 0.1
             
     # partical_optimalization.plot_evolution_of_adaptation()
     # data.plot_decision_boundary(lambda x: neural_network.predict(x))
