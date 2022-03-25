ad7f4b4ea67dadbec453bcc38f13b143bdc0cbab
Kamil Chec <k.chec.prv@gmail.com>
2022-03-25 17:35:58 +0100

    Update paramas

--- src/ecoParams.py
+++ src/ecoParams.py
@@ -1,16 +1,26 @@
-alg_iter = 300
+alg_iter = 1000
+save_to_file = 100
+
+mcp_change_iter = 30
+pso_mode=True
+
 interactions = 5
-save_to_file = 10
+plant_movements = 3
+
 n_pl = 120
 n_he = 60
 n_pr = 40
+
 mutation_pl = 0.01
 mutation_he = 0.1
 mutation_pr = 0.01
-plant_movements = 3
+
+
 life = 5
 size = 2
 nutrition_req = 0.3 # (0.5;1.5)
+
 neighbourhood_r = 1.5 # (-4;4)
-mcp_scope = [-0.5, 2]
-mcp_change_iter = 10
+
+mcp_scope = [-2, 2]
+
