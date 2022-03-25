8d15fb18c373044bccd39bdcc1fbb12991c26390
Kamil Chec <k.chec.prv@gmail.com>
2022-03-25 17:35:34 +0100

    Add new datasets

--- src/dataset.py
+++ src/dataset.py
@@ -6,7 +6,15 @@ import matplotlib.pyplot as plt
 class Dataset:
     def __init__(self):
         np.random.seed(0)
-        self.input, self.output = sklearn.datasets.make_moons(200, noise=0.20)
+        # self.input, self.output = sklearn.datasets.make_moons(200, noise=0.1)
+        self.input, self.output = sklearn.datasets.make_circles(100, noise=0.06)
+        # self.input, self.output = sklearn.datasets.make_blobs(200, centers=4, center_box=(-15,15))
+        # self.input, self.output = sklearn.datasets.make_blobs(200, centers=4)
+        # self.input, self.output = sklearn.datasets.make_classification(
+        #     n_features=2, n_redundant=0, n_informative=2, random_state=1, n_clusters_per_class=1
+        # )
+        # rng = np.random.RandomState(2)
+        # self.input += 2 * rng.uniform(size=self.input.shape)
         
     def get_dataset(self):
         return self.input, self.output
@@ -20,6 +28,6 @@ class Dataset:
         Z = pred_func(np.c_[xx.ravel(), yy.ravel()])
         Z = Z.reshape(xx.shape)
         plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
-        plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Spectral)
+        plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Spectral, edgecolors="k")
+        plt.title('Decision boundary')
         plt.show()
-        
